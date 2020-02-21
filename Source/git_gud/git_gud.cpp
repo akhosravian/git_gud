0b637c53175a9561e69b5ec7e75284d8135df132
Andrew Khosravian <andrew@straybombay.com>
2020-02-21 09:46:52 -0800

    first commit

---
+++ Source/git_gud/git_gud.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "git_gud.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, git_gud, "git_gud" );
---
+++ Source/git_gud/git_gudGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "git_gudGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GIT_GUD_API Agit_gudGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
