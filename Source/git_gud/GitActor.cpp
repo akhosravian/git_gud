3a9dd1238566cd871060b5f42e8297f0789692be
Andrew Khosravian <andrew@straybombay.com>
2020-02-21 20:29:31 -0800

    better comments (#2)

--- Source/git_gud/GitActor.cpp
+++ Source/git_gud/GitActor.cpp
@@ -3,7 +3,7 @@
 
 #include "GitActor.h"
 
-// Sets default values
+// it's a constructor, baby!!
 AGitActor::AGitActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
